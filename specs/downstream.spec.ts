5afe0ecbba62054420135e389050db23b12cb0e1
Abderraouf bendahmane <abderraouf.bendahmene@univ-constantine2.dz>
2026-03-11 14:36:56 +0100

    fixes

--- specs/downstream.spec.ts
+++ specs/downstream.spec.ts
@@ -4,18 +4,18 @@ import payloads from "../helpers/payloads.json";
 
 test.describe("Downstream server", () => {
     test("returns 400 when body lacks user", async ({ request }) => {
-        //given : only donwstream server is up and healthy
-        //when: the downstream service receives a request missing the `user` field
+        // Given : only donwstream server is up and healthy
+        // When: the downstream service receives a request missing the `user` field
         const resp = await loginDirect(request, payloads.missingUser);
-        //then: it should respond with status 400
+        // Then: it should respond with status 400
         expect(resp.status()).toBe(400);
     });
 
     test("echoes back user on valid request", async ({ request }) => {
-        //given : only donwstream server is up and healthy
-        //when: the downstream service receives a well-formed request containing a valid user
+        // Given : only donwstream server is up and healthy
+        // When: the downstream service receives a well-formed request containing a valid user
         const resp = await loginDirect(request, payloads.validUser);
-        //then: it should respond with status 200 and include the user, data, and a token
+        // Then: it should respond with status 200 and include the user, data, and a token
         expect(resp.status()).toBe(200);
         const body = await resp.json();
         expect(body).toHaveProperty("user", payloads?.validUser?.user);
--- specs/proxy.spec.ts
+++ specs/proxy.spec.ts
@@ -4,18 +4,18 @@ import payloads from "../helpers/payloads.json";
 
 test.describe("Proxy service", () => {
     test("returns 400 when request body lacks user", async ({ request }) => {
-        //given: the proxy and downstream server are both running and healthy
-        //When: executing the loginThroughProxy login-related API call with a missing user
+        // Given: the proxy and downstream server are both running and healthy
+        // When: executing the loginThroughProxy login-related API call with a missing user
         const resp = await loginThroughProxy(request, payloads.missingUser);
-        //then: the proxy responds with status 400
+        // Then: the proxy responds with status 400
         expect(resp.status()).toBe(400);
     });
 
     test("forwards valid request and strips user from response and keep other fields", async ({ request }) => {
-        //given: the proxy and downstream server are both running and healthy
-        //when: executing the loginThroughProxy login-related API call with a valid user
+        // Given: the proxy and downstream server are both running and healthy
+        // When: executing the loginThroughProxy login-related API call with a valid user
         const resp = await loginThroughProxy(request, payloads.validUser);
-        //then: the proxy should forward the request, receive the response, and return it with the `user` field stripped out but other fields intact
+        // Then: the proxy should forward the request, receive the response, and return it with the `user` field stripped out but other fields intact
         expect(resp.status()).toBe(200);
         const body = await resp.json();
         expect(body).not.toHaveProperty("user");
@@ -23,25 +23,25 @@ test.describe("Proxy service", () => {
     });
 
     test("responds 400 if downstream response lacks user (dedicated route)", async ({ request }) => {
-        //given: the proxy and downstream server are both running and healthy
-        //when: executing the loginThroughProxyNoUserFromDownStream API call with a valid user payload
+        // Given: the proxy and downstream server are both running and healthy
+        // When: executing the loginThroughProxyNoUserFromDownStream API call with a valid user payload
         const resp = await loginThroughProxyNoUserFromDownStream(request, payloads.validUser);
-        //then: the proxy should respond with status 400 since it requires a `user` field in the downstream response
+        // Then: the proxy should respond with status 400 since it requires a `user` field in the downstream response
         expect(resp.status()).toBe(400);
     });
 
     test("errors when downstream response is not valid JSON", async ({ request }) => {
-        //given: the proxy and downstream server are both running and healthy
-        //when: executing the loginThroughProxyWithInvalidJson API call with a valid user payload
+        // Given: the proxy and downstream server are both running and healthy
+        // When: executing the loginThroughProxyWithInvalidJson API call with a valid user payload
         const resp = await loginThroughProxyWithInvalidJson(request, payloads.validUser);
-        //then: the proxy should fail, and return a 400 error
+        // Then: the proxy should fail, and return a 400 error
         expect(resp.status()).toBeGreaterThanOrEqual(400);
     });
     test("errors when request body is not valid JSON", async ({ request }) => {
-        //given: the proxy and downstream server are both running and healthy
-        //when: executing the loginThroughProxy API call with a plain text payload
+        // Given: the proxy and downstream server are both running and healthy
+        // When: executing the loginThroughProxy API call with a plain text payload
         const resp = await loginThroughProxy(request, payloads.plainText);
-        //then: the proxy should fail, and return a 400 error
+        // Then: the proxy should fail, and return a 400 error
         expect(resp.status()).toBeGreaterThanOrEqual(400);
     });
 });
